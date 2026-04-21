5fd678babb17a7a8c31bdab5f8823719dd0e5807
Dedes <federico.lepore73@gmail.com>
2026-04-21 18:51:31 +0200

    new repo

---
+++ src/main/java/federicolepore/u5w20d2/U5w20d2Application.java
@@ -0,0 +1,13 @@
+package federicolepore.u5w20d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w20d2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w20d2Application.class, args);
+	}
+
+}
---
+++ src/test/java/federicolepore/u5w20d2/U5w20d2ApplicationTests.java
@@ -0,0 +1,13 @@
+package federicolepore.u5w20d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w20d2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
